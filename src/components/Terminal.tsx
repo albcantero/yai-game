b37ee3440ec2c714e6120e6a13f756dc30abc404
Alberto Cantero <alb.cantero@gmail.com>
2026-09-04 18:24:21 +0200

    Curvatura 3D mas ligera: feDisplacementMap scale 45->26 + overscan 1.06->1.035

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -363,7 +363,7 @@ export default function Terminal() {
       <svg width="0" height="0" style={{ position: "absolute" }} aria-hidden="true">
         <filter id="barrel" x="-12%" y="-12%" width="124%" height="124%" colorInterpolationFilters="sRGB">
           <feImage ref={feImageRef} result="map" preserveAspectRatio="none" x="0" y="0" width="100%" height="100%" />
-          <feDisplacementMap in="SourceGraphic" in2="map" scale="45" xChannelSelector="R" yChannelSelector="G" />
+          <feDisplacementMap in="SourceGraphic" in2="map" scale="26" xChannelSelector="R" yChannelSelector="G" />
         </filter>
       </svg>
 
