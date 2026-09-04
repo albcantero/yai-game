e7e4846e4e327eaa428ece479c00d636ad120604
Alberto Cantero <alb.cantero@gmail.com>
2026-09-04 19:06:10 +0200

    Teclado: diseno retroiluminado cian (base oscura, teclas con glow); fuera clase win98 del teclado

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -462,7 +462,7 @@ export default function Terminal() {
       </div>
 
       {showKeyboard && (
-      <div className="win98 keyboard">
+      <div className="keyboard">
         {[
           ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
           ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
