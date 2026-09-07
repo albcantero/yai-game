609278d1e5b1f6def0a1ab8d207fc7f5a459530c
albcantero <alb.cantero@gmail.com>
2026-09-07 23:35:47 +0200

    feat(home): fondo liso teal Win98 (#008080) en vez de la imagen
    - quita el paisaje desktop.png (y el asset) + las variables de posicion: un liso rellena
      cualquier proporcion sin recortes, resuelve el encuadre con/sin teclado y casa con el chrome Win98

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -56,7 +56,6 @@ const Home = forwardRef<ScreenHandle, ScreenServices>(function Home({ playSfx, n
 
   return (
     <div className="home-screen">
-      <img className="home__bg" src="/desktop.png" alt="" />
       <div className="home__content">
         <h1 className="home__title" ref={titleRef}>
           <span className="line">EL libro</span>
